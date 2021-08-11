cd18bab789661e8a7be1b99df84b78b97860de26
Bilal-11 <sbilalahmad2000@gmail.com>
2021-08-11 11:57:35 +0530

    Added the logic to extract digits and compute sum of cubes of digits. Also compiled the source file to create and exe file.

--- Armstrong_num.c
+++ Armstrong_num.c
@@ -4,6 +4,28 @@
 
 int main()
 {
-  int num = 1;
+  int num, digit, num_edit;
+  int sum = 0;
+  for(num=1 ;num<=500; num++)
+  {
+    /*Extract the hundred's digit and add its cube to the sum of cubes of digits*/
+    num_edit = num;
+    digit = num_edit/100;
+    sum += digit * digit * digit;
+
+    /*Extract the ten's digit and add its cube to the sum*/
+    num_edit%=100;
+    digit = num_edit/10;
+    sum += digit * digit * digit;
+
+    /*Extract the one's digit and add it to the sum*/
+    num_edit%=10;
+    digit = num_edit;
+    sum += digit * digit * digit;
+
+    printf("num=%d, sum=%d\n",num,sum);
+
+    sum = 0;
+  }
   return 0;
 }
